594c23ff0e19a371af269aa4c264efa2d7ce39a7
anguschoclab <anguscl@gmail.com>
2026-05-07 11:49:40 +0200

    Update raceGen.ts

--- src/game/raceGeneration/raceGen.ts
+++ src/game/raceGeneration/raceGen.ts
@@ -1,8 +1,8 @@
 import type { Race, RaceClass } from "../types";
-import type { Rng } from "../rng";
-import { nondeterministicRng } from "../rng";
-import type { GradedRace } from "../gradedRaces";
-import { generateUUID } from "../uuid";
+import type { Rng } from "@/game/rng";
+import { nondeterministicRng } from "@/game/rng";
+import type { GradedRace } from "@/core/data/gradedRaces";
+import { generateUUID } from "@/game/uuid";
 import { rand, randomWeather } from "@/core/common/random";
 import { randomTrackConditionWithClimateBias } from "@/core/trackConditions";
 import { randomRaceName } from "@/core/race/naming/legacyFallback";
