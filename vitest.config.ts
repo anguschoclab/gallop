9a9eecffb2c912fa0c5157e5ced2922545a0d3be
anguschoclab <59230472+anguschoclab@users.noreply.github.com>
2026-07-03 10:08:45 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -50,7 +50,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: ["src/tests/setup.ts"],
+    setupFiles: ["@testing-library/jest-dom/vitest", "src/tests/setup.ts"],
     include: ["src/tests/**/*.test.ts", "src/tests/**/*.test.tsx"],
     coverage: {
       provider: "v8",
